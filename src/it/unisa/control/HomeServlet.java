a92d0b0fa77741935db4ae8dac8b56b2b7932c4c
utente <utente@172.19.113.40>
2024-05-27 11:39:32 +0200

    ID-1 resolved

--- src/it/unisa/control/HomeServlet.java
+++ src/it/unisa/control/HomeServlet.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import it.unisa.model.ProdottoBean;
 import it.unisa.model.ProdottoDao;
 
-/**
+/** 
  * Servlet implementation class HomeServlet
  */
 @WebServlet("/home")
